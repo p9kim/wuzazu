479f1ce77c1bd4e98dc3f7bc6001dce6f49d9dfe
Kevin Duke <keduke@UCSD.EDU>
2019-03-04 06:51:55 -0800

    fix if currCell has no North cell it is nullptr

--- wuzazu/HandleEvents.cpp
+++ wuzazu/HandleEvents.cpp
@@ -44,34 +44,34 @@ void EventHandler_::clickInactivePlayer(Cell* clickedCell)
 	prevCell = clickedCell;
 
 	int temp = 0;
-	while (!cellQueue.empty() && temp < 99)
+	while (!cellQueue.empty() && temp < 21)
 	{
 		temp++;
 		currCell = cellQueue.front();
 		cellQueue.pop_front();
 
-		if ((int)weights[currCell->N()].val() < (int)(weights[prevCell].val() - currCell->N()->terrain().weight())) //going from -1 -> 4 or 3 -> 4
+		if (currCell->N() && (int)weights[currCell->N()].val() < (int)(weights[prevCell].val() - currCell->N()->terrain().weight())) //going from -1 -> 4 or 3 -> 4
 			weights[currCell->N()] = weights[prevCell].val() - currCell->N()->terrain().weight();
 		if ((int)weights[currCell->N()].val() > -1)
 		{
 			highlightedCells.push_back(currCell->N());
 			cellQueue.push_back(currCell->N());
 		}
-		if ((int)weights[currCell->S()].val() < (int)(weights[prevCell].val() - currCell->S()->terrain().weight()))
+		if (currCell->S() && (int)weights[currCell->S()].val() < (int)(weights[prevCell].val() - currCell->S()->terrain().weight()))
 			weights[currCell->S()] = weights[prevCell].val() - currCell->S()->terrain().weight();
 		if ((int)weights[currCell->S()].val() > -1)
 		{
 			highlightedCells.push_back(currCell->S());
 			cellQueue.push_back(currCell->S());
 		}
-		if ((int)weights[currCell->E()].val() < (int)(weights[prevCell].val() - currCell->E()->terrain().weight()))
+		if (currCell->E() && (int)weights[currCell->E()].val() < (int)(weights[prevCell].val() - currCell->E()->terrain().weight()))
 			weights[currCell->E()] = weights[prevCell].val() - currCell->E()->terrain().weight();
 		if ((int)weights[currCell->E()].val() > -1)
 		{
 			highlightedCells.push_back(currCell->E());
 			cellQueue.push_back(currCell->E());
 		}
-		if ((int)weights[currCell->W()].val() < (int)(weights[prevCell].val() - currCell->W()->terrain().weight()))
+		if (currCell->W() && (int)weights[currCell->W()].val() < (int)(weights[prevCell].val() - currCell->W()->terrain().weight()))
 			weights[currCell->W()] = weights[prevCell].val() - currCell->W()->terrain().weight();
 		if ((int)weights[currCell->W()].val() > -1)
 		{
