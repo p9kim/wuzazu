e719a87bc0e801805d710f1b4e030c38b014d6aa
Kevin Duke <keduke@UCSD.EDU>
2019-03-04 20:13:59 -0800

    fixed dikstras

--- wuzazu/HandleEvents.cpp
+++ wuzazu/HandleEvents.cpp
@@ -35,48 +35,52 @@ void EventHandler_::clickInactivePlayer(Cell* clickedCell)
 	lastCell = clickedCell;
 	int speed = clickedCell->player()->speed();
 	Cell* currCell = nullptr;
-	Cell* prevCell = nullptr;
 	if (game->currentTeam() != clickedCell->player()->team() || clickedCell->player()->done())
 		return;
 
 	cellQueue.push_back(clickedCell);
 	weights[clickedCell] = *(new Int(speed));
-	prevCell = clickedCell;
 
-	int temp = 0;
-	while (!cellQueue.empty() && temp < 21)
+	while (!cellQueue.empty())
 	{
-		temp++;
 		currCell = cellQueue.front();
 		cellQueue.pop_front();
 
-		if (currCell->N() && (int)weights[currCell->N()].val() < (int)(weights[prevCell].val() - currCell->N()->terrain().weight())) //going from -1 -> 4 or 3 -> 4
-			weights[currCell->N()] = weights[prevCell].val() - currCell->N()->terrain().weight();
-		if ((int)weights[currCell->N()].val() > -1)
+		if (currCell->N() && (int)weights[currCell->N()].val() < (int)(weights[currCell].val() - currCell->N()->terrain().weight())) //check if N cost is less than curCell-N.weight (-1 to 4 or 3 to 4)
 		{
-			highlightedCells.push_back(currCell->N());
-			cellQueue.push_back(currCell->N());
+			weights[currCell->N()] = weights[currCell].val() - currCell->N()->terrain().weight(); //update it with the new weight
+			if ((int)weights[currCell->N()].val() > -1)		//if we're not done (still movements left)
+			{
+				highlightedCells.push_back(currCell->N());	//add the new cell to the highlight list
+				cellQueue.push_back(currCell->N());			//add it to the queue to traverse
+			}
 		}
-		if (currCell->S() && (int)weights[currCell->S()].val() < (int)(weights[prevCell].val() - currCell->S()->terrain().weight()))
-			weights[currCell->S()] = weights[prevCell].val() - currCell->S()->terrain().weight();
-		if ((int)weights[currCell->S()].val() > -1)
+		if (currCell->S() && (int)weights[currCell->S()].val() < (int)(weights[currCell].val() - currCell->S()->terrain().weight())) 
 		{
-			highlightedCells.push_back(currCell->S());
-			cellQueue.push_back(currCell->S());
+			weights[currCell->S()] = weights[currCell].val() - currCell->S()->terrain().weight();
+			if ((int)weights[currCell->S()].val() > -1)
+			{
+				highlightedCells.push_back(currCell->S());
+				cellQueue.push_back(currCell->S());
+			}
 		}
-		if (currCell->E() && (int)weights[currCell->E()].val() < (int)(weights[prevCell].val() - currCell->E()->terrain().weight()))
-			weights[currCell->E()] = weights[prevCell].val() - currCell->E()->terrain().weight();
-		if ((int)weights[currCell->E()].val() > -1)
+		if (currCell->E() && (int)weights[currCell->E()].val() < (int)(weights[currCell].val() - currCell->E()->terrain().weight())) 
 		{
-			highlightedCells.push_back(currCell->E());
-			cellQueue.push_back(currCell->E());
+			weights[currCell->E()] = weights[currCell].val() - currCell->E()->terrain().weight();
+			if ((int)weights[currCell->E()].val() > -1)
+			{
+				highlightedCells.push_back(currCell->E());
+				cellQueue.push_back(currCell->E());
+			}
 		}
-		if (currCell->W() && (int)weights[currCell->W()].val() < (int)(weights[prevCell].val() - currCell->W()->terrain().weight()))
-			weights[currCell->W()] = weights[prevCell].val() - currCell->W()->terrain().weight();
-		if ((int)weights[currCell->W()].val() > -1)
+		if (currCell->W() && (int)weights[currCell->W()].val() < (int)(weights[currCell].val() - currCell->W()->terrain().weight()))
 		{
-			highlightedCells.push_back(currCell->W());
-			cellQueue.push_back(currCell->W());
+			weights[currCell->W()] = weights[currCell].val() - currCell->W()->terrain().weight();
+			if ((int)weights[currCell->W()].val() > -1)
+			{
+				highlightedCells.push_back(currCell->W());
+				cellQueue.push_back(currCell->W());
+			}
 		}
 	}
 	weights.clear();
